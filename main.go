501f84cd617cfa5d29adeae55d3906609a2d5600
Romain Masciulli <romain.masciulli@gmail.com>
2019-05-13 20:09:22 +0200

    Read the configuration file

--- main.go
+++ main.go
@@ -3,12 +3,16 @@ package main
 import (
 	"flag"
 	"fmt"
+	"io/ioutil"
 	"os"
+
+	"gopkg.in/yaml.v2"
 )
 
 func main() {
 	var (
-		help = flag.Bool("help", false, "display the help message")
+		help       = flag.Bool("help", false, "display the help message")
+		configPath = flag.String("config", "config.yaml", "path to the configuration file")
 	)
 
 	flag.Parse()
@@ -16,6 +20,21 @@ func main() {
 	if *help {
 		flag.Usage()
 		os.Exit(0)
+	}
+
+	var config struct {
+	}
+
+	raw, err := ioutil.ReadFile(*configPath)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	err = yaml.Unmarshal(raw, &config)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
 	}
 
 	fmt.Println("sql_exporter")

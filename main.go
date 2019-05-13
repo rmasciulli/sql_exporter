bbf93b24c61ebb79ee5f1909f2ae5f97b8d9e008
Romain Masciulli <romain.masciulli@gmail.com>
2019-05-13 20:02:24 +0200

    Add an help flag

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
 func main() {
+	var (
+		help = flag.Bool("help", false, "display the help message")
+	)
+
+	flag.Parse()
+
+	if *help {
+		flag.Usage()
+		os.Exit(0)
+	}
+
 	fmt.Println("sql_exporter")
 }

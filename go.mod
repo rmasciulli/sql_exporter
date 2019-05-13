9b603bd2e10060c0d2f9a00e3b894a1cc7fdc660
Romain Masciulli <romain.masciulli@gmail.com>
2019-05-13 20:40:32 +0200

    Implement a basic processing loop for queries

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/rmasciulli/sql_exporter
 
 go 1.12
 
-require gopkg.in/yaml.v2 v2.2.2
+require (
+	github.com/go-sql-driver/mysql v1.4.1
+	github.com/jmoiron/sqlx v1.2.0
+	gopkg.in/yaml.v2 v2.2.2
+)
--- main.go
+++ main.go
@@ -5,36 +5,78 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"time"
 
+	"github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
 	"gopkg.in/yaml.v2"
 )
+
+type Config struct {
+	Databases []Database `yaml:"databases"`
+}
+
+type Database struct {
+	Address  string  `yaml:"address"`
+	User     string  `yaml:"user"`
+	Password string  `yaml:"password"`
+	Name     string  `yaml:"name"`
+	Queries  []Query `yaml:"queries"`
+}
+
+type Query struct {
+	Statement string        `yaml:"statement"`
+	Interval  time.Duration `yaml:"interval"`
+	Metric    string        `yaml:"metric"`
+}
 
 func main() {
 	var (
 		help       = flag.Bool("help", false, "display the help message")
 		configPath = flag.String("config", "config.yaml", "path to the configuration file")
 	)
-
 	flag.Parse()
-
 	if *help {
 		flag.Usage()
 		os.Exit(0)
 	}
 
-	var config struct {
-	}
-
+	var config Config
 	raw, err := ioutil.ReadFile(*configPath)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
-
 	err = yaml.Unmarshal(raw, &config)
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
+	}
+
+	for _, database := range config.Databases {
+		conn, err := sqlx.Connect("mysql", (&mysql.Config{
+			Addr:                 database.Address,
+			Net:                  "tcp",
+			User:                 database.User,
+			Passwd:               database.Password,
+			DBName:               database.Name,
+			AllowNativePasswords: true,
+		}).FormatDSN())
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		for _, query := range database.Queries {
+			var result float64
+			err = conn.Get(&result, query.Statement)
+			if err != nil {
+				fmt.Println(err)
+				os.Exit(1)
+			}
+
+			fmt.Println(query.Metric, result)
+		}
 	}
 
 	fmt.Println("sql_exporter")

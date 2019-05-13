638da6db3f4c4aa83a402ac6a33a86f35468db61
Romain Masciulli <romain.masciulli@gmail.com>
2019-05-13 21:08:34 +0200

    Write a monitorQuery routine with a ticker

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/go-sql-driver/mysql"
@@ -53,8 +54,9 @@ func main() {
 		os.Exit(1)
 	}
 
+	var wg sync.WaitGroup
 	for _, database := range config.Databases {
-		conn, err := sqlx.Connect("mysql", (&mysql.Config{
+		db, err := sqlx.Connect("mysql", (&mysql.Config{
 			Addr:                 database.Address,
 			Net:                  "tcp",
 			User:                 database.User,
@@ -68,16 +70,31 @@ func main() {
 		}
 
 		for _, query := range database.Queries {
-			var result float64
-			err = conn.Get(&result, query.Statement)
-			if err != nil {
-				fmt.Println(err)
-				os.Exit(1)
-			}
-
-			fmt.Println(query.Metric, result)
+			wg.Add(1)
+			go func(db *sqlx.DB, query Query) {
+				monitorQuery(db, query)
+				wg.Done()
+			}(db, query)
 		}
 	}
+	wg.Wait()
 
 	fmt.Println("sql_exporter")
+}
+
+func monitorQuery(db *sqlx.DB, query Query) {
+	t := time.NewTicker(query.Interval)
+
+	var result float64
+	for {
+		now := <-t.C
+
+		err := db.Get(&result, query.Statement)
+		if err != nil {
+			fmt.Println(err)
+			os.Exit(1)
+		}
+
+		fmt.Println(now, query.Metric, result)
+	}
 }

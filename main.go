8ca558a3c630655520c405aafa52e081f56c747f
Romain Masciulli <romain.masciulli@gmail.com>
2019-06-03 20:43:53 +0200

    Implement a gracefull shutdown

--- main.go
+++ main.go
@@ -1,10 +1,12 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"io/ioutil"
 	"os"
+	"os/signal"
 	"sync"
 	"time"
 
@@ -48,6 +50,15 @@ func main() {
 		os.Exit(1)
 	}
 
+	ctx, cancel := context.WithCancel(context.Background())
+	c := make(chan os.Signal)
+	signal.Notify(c, os.Interrupt, os.Kill)
+	go func() {
+		<-c
+		fmt.Println("\nmonitorQuery() stopped")
+		cancel()
+	}()
+
 	var wg sync.WaitGroup
 	for _, database := range config.Databases {
 		db, err := sqlx.Connect("mysql", (&mysql.Config{
@@ -66,12 +77,13 @@ func main() {
 		for _, query := range database.Queries {
 			wg.Add(1)
 			go func(db *sqlx.DB, query Query) {
-				monitorQuery(db, query)
+				monitorQuery(ctx, db, query)
 				wg.Done()
 			}(db, query)
 		}
 	}
 	wg.Wait()
+	fmt.Println("the program was gracefully shutdowned")
 }
 
 func loadConfiguration(path string) (c Configuration, err error) {
@@ -91,17 +103,22 @@ func loadConfiguration(path string) (c Configuration, err error) {
 	return c, nil
 }
 
-func monitorQuery(db *sqlx.DB, query Query) {
+func monitorQuery(ctx context.Context, db *sqlx.DB, query Query) {
 	t := time.NewTicker(query.Interval)
 
 	var result float64
+	var now time.Time
 	for {
-		now := <-t.C
+		select {
+		case <-ctx.Done():
+			return
+		case now = <-t.C:
+		}
 
-		err := db.Get(&result, query.Statement)
+		err := db.GetContext(ctx, &result, query.Statement)
 		if err != nil {
 			fmt.Println(err)
-			os.Exit(1)
+			return
 		}
 
 		fmt.Println(now, query.Metric, result)
